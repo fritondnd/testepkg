b6f8dbea3b275c46f0f0db5c3a258d45ba1a42e7
Gilson Lima <frito.login@gmail.com>
2022-01-22 14:36:14 -0300

    Initial commit

---
+++ R/somar.R
@@ -0,0 +1,15 @@
+#' somar duas parcelas x e y
+#'
+#' @param x parcela x
+#' @param y parcela y
+#'
+#' @return retorna soma das parcelas x e y
+#' @export
+#'
+#' @examples
+#'
+#' somar(3,5)
+somar <- function(x, y){
+  retorno <- x + y
+  return(retorno)
+}
